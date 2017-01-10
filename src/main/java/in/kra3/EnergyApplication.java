1493fdfb7a39a996a3d4d0e3e00821cfaf93167f
Arun K R <the1.arun@gmail.com>
2017-01-10 09:46:22 +0530

    Springboot starter with jpa, h2 & rest

---
+++ src/main/java/in/kra3/EnergyApplication.java
@@ -0,0 +1,12 @@
+package in.kra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyApplication.class, args);
+	}
+}
